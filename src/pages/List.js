1efe2b7f0a955239c70c9d4e06003cb8ce44ea30
Maisa Maximo <maisa_maximo@outlook.com>
2019-10-04 17:38:20 -0300

    add booking page

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, AsyncStorage, Image } from 'react-native';
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png'
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container} >
+            <Image style={styles.logo} source={logo} /> 
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech}/>)}    
+            </ScrollView>      
+        </SafeAreaView>
+        );
 }
+
+const styles = StyleSheet.create( {
+    container: {flex: 1,},
+    logo: {height: 32, resizeMode: "contain", alignSelf: "center", marginTop: 10,},
+})
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
         View,
         KeyboardAvoidingView,
@@ -17,6 +17,14 @@ import logo from '../assets/logo.png'
 export default function Login({ navigation }) {
     const [email, setEmail] = useState('');
     const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('List');
+            }
+        })
+    }, []);
 
     async function handleSubmit() {
         const response = await api.post('/sessions', { email });
